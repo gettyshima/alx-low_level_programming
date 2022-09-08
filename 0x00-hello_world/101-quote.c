89113e5343169a173d1bd03070bd8a73c1764c73
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-08 06:18:04 -0700

    make file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("and that piece of art is useful%c %c ", '"', '-');
+	printf(" Dora Korpar, 2015%c10%c19%c", '-', '-', 10);
+		return (1);
+}
