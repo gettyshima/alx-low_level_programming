f0c322f27d26648508355ae1e9180e7cef9a2cee
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-08 05:11:46 -0700

    create fike himBHsputs.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+	return (0);
+}
+
