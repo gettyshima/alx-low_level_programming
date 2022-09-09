4e96624662850308d923a5c3de691fe02770601d
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-09 05:38:13 -0700

    make file 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  *main - prints alphabets in lowercase except q and e
+  *
+  *Return: 0 (success)
+  */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; ++alpha)
+	{
+		if (alpha == q)
+			continue;
+
+		else if (alpha == q)
+			continue;
+	}
+	putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
