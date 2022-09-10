a5ecf1f43a47cc6e3e0b91beee2878f954c07ace
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-09 20:06:52 -0700

    make file 102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+  *main - prints all possible combination of two two digit
+  *
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	char i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if (i == k && j == l)
+						continue;
+					if (i == 57 && j == 57)
+						continue;
+					if (j != l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+				if (i == 57 && j == 56 && k == 57 && l == 57)
+					break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
