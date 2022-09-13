a4873ec614275646749a4c689d715b5034148f34
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 11:40:00 -0700

    make file 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - prints natural numbers from n to 98
+ *@n: any number passes
+ *
+ *Return: 0 (success)
+ */
+void print_to_98(int n)
+{
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ");
+		if (i == 98)
+			printf("98");
+		return (0);
+	}
+}
