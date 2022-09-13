1bbc8da695ddd4613981cbf8ee9f093989f7886a
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 08:28:16 -0700

    update file 7-print_last_dugit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,25 +2,26 @@
 /**
   *print_last_digit - prints the last digit of a number
   *@n: value of integer entered
-  *@last_dig: value of last digit calculated
   *
   *Return: the last digit of the last digit
   */
 int print_last_digit(int n)
 {
-	int last_dig;
-
 	if (n < 0)
 	{
 		n = (-1) * n;
-		last_dig = n % 10;
-		_putchar(last_dig + '0');
+		n = n % 10;
+		_putchar(n + '0');
 	}
-	else if (n >=0)
+	else if (n >= 0)
 	{
-		last_dig = n % 10;
-		_putchar(last_dig + '0');
+		n = n % 10;
+		_putchar(n + '0');
 	}
-	
-	return (last_dig);
+	else if (n == -2147483648)
+	{
+		n--;
+		_putchar(n + '0');
+	}
+	return (n);
 }
