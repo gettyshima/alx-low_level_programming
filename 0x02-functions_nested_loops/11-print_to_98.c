810d13055f5f58512b4063a462e2311a7d244b0e
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 11:44:07 -0700

    update file 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,13 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	for (i = n; i <= 98; i++)
 	{
-		printf("%d, ");
+		printf("%d, ", i);
 		if (i == 98)
-			printf("98");
+			printf("98\n");
 		return (0);
 	}
 }
