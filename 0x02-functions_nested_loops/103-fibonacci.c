ba4190e3c850a6b6b1bb3bce09deaf9bba408640
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 16:40:28 -0700

    create file 103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - find and add all even value of fabanacci numbers
+  *that do not exceed 4000000
+  *
+  *Return: 0 (success)
+  */
+int main(void)
+{
+	int n;
+	long int i = 0, j = 1, add, sum = 0;
+
+	while (add < 4000000)
+	{
+		if (add % 2 == 0)
+			sum = sum + add;
+		add = i + j;
+		i = j;
+		j = add;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
