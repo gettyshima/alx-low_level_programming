2d4e795b794108d0fe0a222bc26d71a449a6ba5d
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 04:44:39 -0700

    make file 3-lower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_islower - checks if character is lower case or not
+  *@c: any character passed
+  *
+  *Return: 1 if c is lowercase and 0 otherwise
+  */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
