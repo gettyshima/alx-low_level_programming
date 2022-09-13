e09fb1d587af624f3a8324b01a2d1b7c0d21d9b6
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 11:10:46 -0700

    create file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *add - adds two integer and returns the result
+  *@x: first integer
+  *@y: second integer
+  *int: values entered as int
+  *
+  *Return: the result
+  */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = int + int;
+	return (sum);
+}
+
