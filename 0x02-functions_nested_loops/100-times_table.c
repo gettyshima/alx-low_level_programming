fef759286349e1e57b40db4a31e5f5f362a06dd1
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-13 20:17:18 -0700

    create file 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,58 +1,50 @@
-#include "main.h"
+ #include "main.h"
+
 /**
-  *print_times_table - prints n times tables, starting from 0
-  *@n: the number of multiplication
-  *
-  *Return: void
-  */
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 void print_times_table(int n)
 {
-	int i, j, mul;
+	int x = 0, y, z;
 
-	for (i = 0; i <= n; i++)
+
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (j = 0; j <= n; j++)
+		for (y = 0; y <= n; y++)
 		{
-			if (!(n > 15 || n < 0))
+			z = x * y;
+			if (z > 99)
 			{
-				mul = j * i;
-				if (j == 0)
-				{
-					_putchar('0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if (mul < 10)
-				{
-					_putchar(' ');
-					_putchar(mul + '0');
-					if (j == n)
-						break;
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if (mul >= 10 && mul < 100)
-				{
-					_putchar(mul / 10 + '0');
-					_putchar(mul % 10 + '0');
-					if (j == n)
-						break;
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if (mul >= 100)
-				{
-					_putchar(mul / 100 + '0');
-					_putchar((mul % 100) / 100 + '0');
-					_putchar((mul % 100) % 10 + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
+		x++;
 	}
-}
+} 
