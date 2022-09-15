1d18fa010957593e8c78a56f4ca3a2076f6ad88b
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-15 07:31:35 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,17 +9,17 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = '0';
+	i = 0;
 
-	while (i <= '9')
+	while (i <= 9)
 	{
-		if (i == '2' || i == '4')
+		if (i == 2 || i == 4)
 		{
 			i++;
 			continue;
 		}
 
-		_putchar(i);
+		_putchar(i + '0');
 		i++;
 	}
 
