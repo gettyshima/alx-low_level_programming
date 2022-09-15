4339e5d3c4416f664543c7b704f2845352153d07
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-15 15:09:37 -0700

    create file 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,12 +16,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && (i % 3 != 0))
-			printf("Buzz ");
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
 		else if (i % 3 == 0 && (i % 5 != 0))
 			printf("Fizz ");
-		else if (i % 15 == 0)
-			printf("FizzBuzz ");
+		else if (i % 5 == 0 && (i % 3 != 0))
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
