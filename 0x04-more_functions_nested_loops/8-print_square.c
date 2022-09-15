014daf7ed4b8c3a603f45567ebdbdba10c1f0adc
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-15 14:22:28 -0700

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,11 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (!(n <= 0))
+	if (!(size <= 0))
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= n; j++)
+			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
