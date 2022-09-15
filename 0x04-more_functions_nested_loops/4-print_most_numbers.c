c62a4843b0b68bca1c6dcd117983a9c59d9ec4b4
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-15 07:07:04 -0700

    create file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints number from 0 to 9 with exception of 2 and 4
+  * followed by a new line
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= '9')
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
