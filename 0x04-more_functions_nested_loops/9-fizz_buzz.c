144dc39b5c5fd8c6230d6d89830debb7ac8d88fd
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-15 15:32:20 -0700

    create file 9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
-  *main - function block
-  *Description: prints numbers from 1 to 100, but for multiples of 3
+  *main - Entry block
+  *prints numbers from 1 to 100, but for multiples of 3
   *print fizz, and print buzz for multiples of 5
   *for both print fizzbuzz
   *
@@ -17,13 +16,21 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	putchar('\n');
 	return (0);
