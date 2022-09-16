5c11a9c58941665c6d7bd31ba7a8fdcae666337e
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-16 09:52:57 -0700

    update file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  *main - prints the first 50 fibonnacci numbers starting with 1 and 2
+  *main - prints the first 50 fibonnacci numbers starting with 1 and 2.
   *
   *Return: 0 (success)
   */
