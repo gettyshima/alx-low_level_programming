c5d9541b49e12f2816bb2261d8de37c54e2d28e0
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-19 06:19:12 -0700

    create file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_rev - prints a string in reverse followed by a newline
+  *@s: the string to be printed
+  *
+  *Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; i[s] != '\0'; i++)
+	{
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
