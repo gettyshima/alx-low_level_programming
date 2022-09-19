336b752885c82e805cab2f74cafefaebfbcf3373
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-19 08:54:05 -0700

    create file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+include "main.h"
+
+/**
+  *puts_half - prints the second half of the string
+  *if number of character is odd, print last character
+  *@str: character to be printed
+  *
+  *Return: void
+  */
+
+void puts_half(char *str)
+{
+	int i, j, k, l;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = 0; j < i; j++)
+	{
+		if (i % 2 == 0)
+		{
+			k = i / 2;
+			_putchar(str[j + k]);
+		}
+		else if (i % 2 != 0)
+		{
+			l = i / 2 + 1;
+			_putchar (str[j + l]);
+		}
+	}
+	_putchar('\n');
+}
+
+
