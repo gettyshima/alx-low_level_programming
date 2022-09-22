8b8c506bc75b78417658fe1463e2e07430ae392e
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-21 17:18:47 -0700

    update file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,27 @@
 #include "main.h"
+
+/**
+  *leet - encodes a string to 1337
+  *@s: string to be encoded
+  *
+  *Return: s
+  */
+
+char *leet(char *s)
+{
+	int i, j;
+	char upper[5] = "AEOTL";
+	char lower[5] = "aeotl";
+	char nums[5] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == upper[j] || s[i] == lower[j])
+				s[i] = nums[j];
+		}
+	}
+	return (s);
+}
+
