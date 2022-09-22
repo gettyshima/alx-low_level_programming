50957ab6a06af7e97a5ec4715997c8cd32b9370a
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-22 05:25:25 -0700

    update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+  *rot13 - encodes a string using rot 13
+  *@s: string to be encoded
+  *
+  *Return: s
+  */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alp1[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp2[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alp1[j])
+			{
+				s[i] = alp2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
