dbc2b25d8dab8951406844e0e7cd6389b0e23737
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-22 05:47:12 -0700

    update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+  *print_number - prints interger
+  *@n: number to be printed
+  *
+  *Return: void
+  */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+		m = n;
+	if (m / 10)
+		print_number(m / 10);
+	_putchar(m % 10 + '0');
+}
