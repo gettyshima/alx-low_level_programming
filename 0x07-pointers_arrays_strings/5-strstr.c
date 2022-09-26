436759a5af18bb4831f16f8114dc56447d0544ca
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-26 12:47:54 -0700

    create file 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *_strstr - finds first occurance of a substring in a string
+  *@haystack: the string
+  *@needle: the substring
+  *
+  *Return: a pointer to the begining of the substring
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		for (i = 0; haystack[i] != '\0'; i++)
+		{
+			if (needle[j] == haystack[i])
+				return (&haystack[i]);
+		}
+	}
+	return (0);
+}
