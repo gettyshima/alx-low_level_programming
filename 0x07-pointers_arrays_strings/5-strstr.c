5ce27b1ae4600aa0f2ac3ac5a97cb912e86b70eb
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-26 16:46:31 -0700

    create file 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   *_strstr - finds first occurance of a substring in a string
@@ -7,18 +8,79 @@
   *
   *Return: a pointer to the begining of the substring
   */
-
+/*
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (j = 0; needle[j] != '\0'; j++)
+	while (*haystack != '\0')
 	{
-		for (i = 0; haystack[i] != '\0'; i++)
+		haystack++;
+
+	return (0);
+}
+
+
+#include "main.h"
+#include <stdio.h>
+*/
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i] && needle[j] != '\0')
-				return (&haystack[i]);
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (0);
+}
+/*
+   #include "main.h"
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *need = needle;
+		char *hay = haystack;
+
+		while (*need != '\0' && *hay == *need)
+		{
+			hay++;
+			need++;
+		}
+
+		if (*need == '\0')
+		{
+			return (haystack);
 		}
 	}
 	return (0);
+}
+
+*/
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "held";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_chessboard - prints a chessboard
+  *@a:name of array
+  *Return: void
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
+int main(void)
+{
+    char board[8][8] = {
+        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    };
+    print_chessboard(board);
+    return (0);
+}
