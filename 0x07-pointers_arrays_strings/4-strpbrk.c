d06445a87b3129529171ffc81f4f790a9594793a
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-26 10:26:08 -0700

    create file 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   *_strpbrk - locates the first occurance in a string
-  *@s: the string 
+  *@s: the string
   *@accept: the string to be located
   *
   *Return: s
@@ -23,22 +22,3 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (0);
 }
-	int main(void)
-
-	{
-
-		    char *s = "hello, world";
-
-		        char *f = "world";
-
-			    char *t;
-
-
-
-			        t = _strpbrk(s, f);
-
-				    printf("%s\n", t);
-
-				        return (0);
-
-	}
