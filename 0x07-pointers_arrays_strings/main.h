23d8c94d4b3ae870d8e38c25978340d01d1f0a92
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-26 16:52:31 -0700

    update file 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-int _putchaf(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
