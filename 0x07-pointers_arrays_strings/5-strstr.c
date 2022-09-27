c08d6e2673b75b19d85c4945dcef5ef15af17508
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-26 20:49:01 -0700

    update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   *_strstr - finds first occurance of a substring in a string
@@ -8,49 +7,6 @@
   *
   *Return: a pointer to the begining of the substring
   */
-/*
-char *_strstr(char *haystack, char *needle)
-{
-	int i, j;
-
-	while (*haystack != '\0')
-	{
-		haystack++;
-
-	return (0);
-}
-
-
-#include "main.h"
-#include <stdio.h>
-*/
-/**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for
- *
- * Return: pointer to the beginning of the located substring
- * or NULL if the substring is not found
- */
-char *_strstr(char *haystack, char *needle)
-{
-	int i, j;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (&haystack[i]);
-	}
-	return (0);
-}
-/*
-   #include "main.h"
-
 
 char *_strstr(char *haystack, char *needle)
 {
@@ -70,17 +26,5 @@ char *_strstr(char *haystack, char *needle)
 			return (haystack);
 		}
 	}
-	return (0);
 }
 
-*/
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "held";
-    char *t;
-
-    t = _strstr(s, f);
-    printf("%s\n", t);
-    return (0);
-}
