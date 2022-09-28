f8151c5c07b8ce96753b997ac2e579ec3f46c735
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-09-28 06:43:47 -0700

    create file 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_print_rev_recursion - prints string in reverse
+  *@s: string to be printed
+  *
+  *Return: nothing
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
+
