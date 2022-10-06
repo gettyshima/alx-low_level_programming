5a04ebc33cf7121f5f2d6a71e86c04a51218c2c9
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-06 10:43:45 -0700

    create file 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,6 +17,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min + 1;
 	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = (min + i);
 	return (arr);
