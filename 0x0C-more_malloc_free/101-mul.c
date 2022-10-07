9cc7fca9088100b4f5ae815eb38592109d4b617e
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-06 18:06:34 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,15 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int real_mul(un
+
+/**
+  *print_error - prints error
+  *Return: nothing
+  */
+void print_error(void)
+{
+	char *s = "Error\n";
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		_putchar(*(s + i));
+}
+
+/**
+  *main _ multiplies teo numbers
+  *@argc: number of argument
+  *@argv: atgument passed
+  *
+  *Return: 0 success
+  */
 int main (int argc, char *argv[])
 {
 	unsigned  long int num1, num2, mul;
 	int i, j;
+	char c[] = "Error\n";
 
-	if (argc < 2 || argc > 4)
+	if (argc < 2 || argc > 3)
 	{
-		printf("Error\n");
+		for (i = 0; i != '\0'; i++)
+			_putchar (c[i]);
 		exit (98);
 	}
 	for (i = 1; i < argc; i++)
@@ -19,14 +41,15 @@ int main (int argc, char *argv[])
 		{
 			if (!(argv[i][j]  >= '0' && argv[i][j] <= '9'))
 			{
-				printf("Error\n");
+				for (i = 0; i != '\0'; i++)
+					_putchar (c[i]);
 				exit (98);
 			}
 		}
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	mul = num1 + num2;
+	mul = num1 * num2;
 
 	printf("%lu\n", mul);
 	return (0);
