3806414f76f36f837464b59b7ae2061912fbe23f
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-06 17:03:01 -0700

    update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		memcpy(newmem, ptr, old_size);
-		newmem[old_size] = 0;
 		free(ptr);
 	}
 	return (newmem);
