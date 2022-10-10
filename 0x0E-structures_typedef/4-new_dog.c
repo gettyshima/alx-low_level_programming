ff911694f50deba6fb48d23272cc88e1870ea6b0
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-10 06:50:11 -0700

    create 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,25 +16,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	dog->name = malloc(sizeof(char) * strlen(name) + 1);
-	if (dog->name == NULL)
+	if (!(name == NULL && owner == NULL))
 	{
-		free(dog);
-		return (NULL);
+		dog = malloc(sizeof(dog_t));
+		if (dog == NULL)
+			return (NULL);
+		dog->name = malloc(sizeof(char) * strlen(name) + 1);
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+		dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+		if (dog->owner == NULL)
+		{
+			free(dog->name);
+			free(dog);
+			return (NULL);
+		}
+		dog->name = strcpy(dog->name, name);
+		dog->age = age;
+		dog->owner = strcpy(dog->owner, owner);
 	}
-	dog->owner = malloc(sizeof(char) * strlen(owner) + 1);
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	dog->name = strcpy(dog->name, name);
-	dog->age = age;
-	dog->owner = strcpy(dog->owner, owner);
 
 	return (dog);
 }
