87db2c75de0de712ffe26d7f7bb4a7fa3139c37e
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-10 04:20:34 -0700

    create file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - structs with elements
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: owner of the dog
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
