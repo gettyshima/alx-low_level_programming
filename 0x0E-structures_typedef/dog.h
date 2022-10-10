02bfc2320eaf9df720c64e0a76a1abccc476ca76
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-10 05:50:33 -0700

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,9 @@ struct dog
 /**
   *dog_t - typedef for struct dog
   */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 
 #endif
