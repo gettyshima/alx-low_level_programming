cb6eb43c126ab0a45db6a12259488da710081558
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-10 05:41:28 -0700

    create file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,13 +10,12 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		;
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = (nil);
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (!(d == NULL))
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
 }
