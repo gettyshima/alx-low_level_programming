38aba3259efc519c149ccc1234527789310c29a8
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-10 05:24:01 -0700

    create file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,14 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
+  *print_dog - prints struct dog
+  *@d: the name of the struct to be printed
   *
+  *Return: nothing
+  */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
