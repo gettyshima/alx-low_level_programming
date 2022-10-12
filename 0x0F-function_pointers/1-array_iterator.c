a291452f8e66110ba142a92d937a1ca49cf4adff
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-12 04:18:33 -0700

    update files

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || action == NULL || size <= 0)
+		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,6 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
 	if (cmp != 0)
 	{
 		for (i = 0; i < size; i++)
