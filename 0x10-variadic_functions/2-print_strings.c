b854a6e80036e4e7b7d23979d6678ac46218a00c
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-13 07:21:43 -0700

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (!(i == (n - 1)))
 				printf("%s", separator);
 		}
+		printf("\n");
 	}
-	printf("\n");
 	va_end(ac);
 }
