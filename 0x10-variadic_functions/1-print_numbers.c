f3ca2f4abb45327465cd06ccb82f1828e81b98d3
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-13 06:58:30 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (!(i == (n - 1)))
 				printf("%s", separator);
 		}
+		printf("\n");
 	}
-	printf("\n");
 	va_end(ac);
 }
