9e259c082af3b45244edde47206aea8bd4856c8a
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-13 17:22:20 -0700

    create file 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,73 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
 
+void print_separator(int n, int len, const char * const string);
+
 /**
-  *void print_all(const char * const format, ...);
-  *
+  *print_all - prints anything
+  *@format: list of types to be printed
+  *Return: nothing
+  */
+
+void print_all(const char * const format, ...)
+{
+	int i, arg_count;
+	char *str;
+	va_list my_list;
+
+	va_start(my_list, format);
+	arg_count = strlen(format);
+	i = 0;
+	while (i < arg_count)
+	{
+		switch (format[i])
+		{
+			{ case 'c': printf("%c", va_arg(my_list, int)); }
+				break;
+			{ case 'i': printf("%d", va_arg(my_list, int)); }
+				break;
+			{ case 'f': printf("%f", va_arg(my_list, double)); }
+				break;
+			case 's':
+				{
+					str = va_arg(my_list, char *);
+					if (str == NULL)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s", str);
+					break;
+				}
+		}
+		print_separator(i, arg_count, format);
+		i++;
+	}
+	printf("\n");
+	va_end(my_list);
+}
+
+/**
+  *print_separator - print the separator
+  *@n: the iterator number
+  *@len: lenght of string
+  *@string: the string
+  *Return: nothing
+  */
+
+void print_separator(int n, int len, const char * const string)
+{
+	if (n < len - 1)
+	{
+		switch (string[n])
+		{
+			{ case 'c': printf(", "); }
+				break;
+			{ case 'i': printf(", "); }
+				break;
+			{ case 'f': printf(", "); }
+				break;
+			{ case 's': printf(", "); }
+				break;
+		}
+	}
+}
