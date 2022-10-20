9efd9ef0e63c74a150b27ca0437a49e2183b248a
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-20 06:23:10 -0700

    create file 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,4 +10,17 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	char *dup;
+	list_t *ptr = malloc(sizeof(list_t));
 
+	if (ptr == NULL)
+		return (NULL);
+	dup = strdup(str);
+	ptr->len = strlen(str);
+	ptr->str = dup;
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
