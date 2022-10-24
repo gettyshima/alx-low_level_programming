c10d74e636057f834a4b5363a0f44e2743ef89f0
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-24 11:31:09 -0700

    create file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *print_listint -  prints all elements of listint_t
+  *@h: head link
+  *
+  *Return: number of nodes
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
