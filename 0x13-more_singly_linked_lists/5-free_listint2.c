e1bdc84f5a20b9300e2353cd553cf6eeb4ca9aa3
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-24 14:27:18 -0700

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -23,5 +23,5 @@ void free_listint2(listint_t **head)
 		free(current);
 	}
 	free(*head);
-	*head = NULL;
+	head = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  *int pop_listint(listint_t **head)- deletes head node of listint_t
+  *@head: head list
+  *
+  *Return: head node’s data (n)
+  *or 0 of list is empty
+  */
+
+int pop_listint(listint_t **head)
+{
+
