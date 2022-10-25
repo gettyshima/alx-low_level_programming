19a407b7cd0746cf37610654a4906c1b5822b105
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-25 07:43:21 -0700

    update free file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
 	listint_t *temp2;
 
 	temp2 = *head;
-	while (temp2 != NULL && head != NULL)
+	while (*head != NULL && head != NULL)
 	{
 		temp = temp2;
 		temp2 = temp2->next;
