432e2e19373b15a25807e9ff37c18adb82c67433
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-27 07:36:46 -0700

    create file 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,10 +5,14 @@
   *@n: the number
   *@index: the index
   *
-  *Return: value of the bit at index index 
-  *or -1 if an error occured
+  *Return: value of the bit at index index 
+  *or -1 if an error occured
   */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+	if (index > 64)
+		return (-1);
+	n >>= index;
+	return (n & 1);
+}
