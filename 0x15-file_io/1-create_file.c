b023be1b91d2ae6116bcb71b1e35908b7a9d271f
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-10-31 14:30:56 -0700

    update file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
-  *_strlen - finds strlen
-  *@s: the string
-  *
-  *Return: string lenght
-  */
-
-int _strlen(char *s)
-{
-	int i, len = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-		len++;
-	return (len);
-}
-/**
   *create_file - creates a file
   *@filename: name of file to create
   *@text_content: strings to write
@@ -29,12 +14,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC,  0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
 	{
-		w = write(fd, text_content, _strlen(text_content));
+		w = write(fd, text_content, strlen(text_content));
 		if (w == -1)
 			return (-1);
 	}
