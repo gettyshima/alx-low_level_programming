222a90823eadcc6160427c574afbcfea71bf0022
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-12-12 09:01:41 -0800

    update

--- 0x18-dynamic_libraries/tmp/0-putchar.c
+++ 0x18-dynamic_libraries/tmp/0-putchar.c
@@ -1,23 +1,12 @@
-#include "main.h"
+#include <unistd.h>
 /**
-  *main - prints the string _putchar
-  *
-  *Return: 0 (success)
-  */
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
-
-
-
