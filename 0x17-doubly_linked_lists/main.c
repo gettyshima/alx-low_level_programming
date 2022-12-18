013dbb492363ba26d7aa85b294e65eccb6c491b2
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-12-18 09:52:53 +0100

    update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -11,6 +8,7 @@
 int main(void)
 {
     dlistint_t *head;
+    int sum;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -21,6 +19,9 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
+    sum = sum_dlistint(head);
+    printf("sum = %d\n", sum);
+    free_dlistint(head);
+    head = NULL;
     return (EXIT_SUCCESS);
 }
