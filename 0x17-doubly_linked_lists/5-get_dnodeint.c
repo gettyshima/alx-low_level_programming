78f25038f3ec2f1f8801819bd707d2cab37b7722
Gertrude Ahemba <gertrudeahemba@gmail.com>
2022-12-18 00:45:06 -0800

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	size_t count = 0;
 
-	while((count < index) && (head != NULL))
+	while ((count < index) && (head != NULL))
 	{
 		head = head->next;
 		count++;
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  *sum_dlistint - suma all data(n) of dlistint
+  *@head: head pointer
+  *
+  *Return: the sum or 0 if empty
+  */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
